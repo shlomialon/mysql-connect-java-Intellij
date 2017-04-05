603590b38db14d513b2fd1107d2b7c850210e960
shlomialon <shlomi.al90@gmail.com>
2017-04-05 20:20:58 +0300

    insert a connecting code

---
+++ README.md
@@ -0,0 +1,1 @@
+"# mysql-connect-java-Intellij" 
---
+++ mysqlTest/src/Main.java
@@ -0,0 +1,22 @@
+/**
+ * Created by Shlomi Alon on 05/04/2017.
+ */
+import java.sql.*;
+public class Main{
+    public static void main(String[] args){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            try(Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/northwind", "root", "shlomi2249")){
+                Statement stmt = con.createStatement();
+                ResultSet rs = stmt.executeQuery("SELECT * FROM products");
+                int numOfColumns = rs.getMetaData().getColumnCount();
+                while (rs.next()){
+                    for (int col = 1; col <= numOfColumns; col++){
+                        System.out.print(rs.getString(col) + "\t" + "|" + "\t");
+                    }
+                    System.out.println();
+                }
+            }} catch (Exception ex){ex.printStackTrace();}
+    }
+}
+
